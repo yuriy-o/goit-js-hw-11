cee5ddfad60854e11b34fba5b5ae3c4d37e2deb3
yuriy-o <82809592+yuriy-o@users.noreply.github.com>
2022-12-08 11:38:26 +0000

    Deploying to gh-pages from @ yuriy-o/goit-js-hw-11@ac2b87c06891e1c39ef2363c6f48b5fa4af638af 🚀

--- index.6ccac2c9.js
+++
@@ -1,3 +0,0 @@
-//! DELETE
-fetch("https://jsonplaceholder.typicode.com/posts/1",{method:"DELETE"}).then((()=>console.log("Post deleted"))).catch((o=>console.log("Error:",o)));
-//# sourceMappingURL=index.6ccac2c9.js.map
--- index.b2b3ebd8.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/posts/".concat(1),{method:"DELETE"}).then((function(){return console.log("Post deleted")})).catch((function(o){return console.log("Error:",o)}));
-//# sourceMappingURL=index.b2b3ebd8.js.map
