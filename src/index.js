5b0e01c069e854eb732fabc3494f3879bd58d1ee
Yuriy <sharik.kiev@gmail.com>
2022-12-09 10:49:59 +0200

    commit using the console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+// //! GET
+// const postToAdd = {
+//   author: 'Mango',
+//   body: 'CRUD is awesome',
+// };
+
+// const options = {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// fetch('https://jsonplaceholder.typicode.com/posts', options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// //! POST
+// const postToAdd = {
+//   author: 'Mango',
+//   body: 'CRUD is awesome',
+// };
+
+// const options = {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// fetch('https://jsonplaceholder.typicode.com/posts', options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// //! DELETE
+// const postIdToDelete = 1;
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
+//   method: 'DELETE',
+// })
+//   .then(() => console.log('Post deleted'))
+//   .catch(error => console.log('Error:', error));
+
 {
   /* <button type="button" class="load-more">Load more</button> */
 }
