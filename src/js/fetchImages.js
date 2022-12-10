10bcbdc8be38ea083d32d9738fb915bd622bd554
Yuriy <sharik.kiev@gmail.com>
2022-12-10 21:37:13 +0200

    axois to fetch

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,34 +1,34 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export function fetchImages(searchQuery) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '17067261-d096522309c4f7c33a0f4f98e';
-
-  return axios
-    .get(
-      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-    )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      return response.data;
-    });
-}
-
-//! Base API on fetch
 // export function fetchImages(searchQuery) {
 //   const BASE_URL = 'https://pixabay.com/api/';
 //   const API_KEY = '17067261-d096522309c4f7c33a0f4f98e';
 
-//   return fetch(
-//     `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-//   ).then(resp => {
-//     if (!resp.ok) {
-//       throw new Error(resp.status);
-//     }
+//   return axios
+//     .get(
+//       `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
+//     )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
 
-//     return resp.json();
-//   });
+//       return response.data;
+//     });
 // }
+
+//! Base API on fetch
+export function fetchImages(searchQuery) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '17067261-d096522309c4f7c33a0f4f98e';
+
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.status);
+    }
+
+    return resp.json();
+  });
+}
